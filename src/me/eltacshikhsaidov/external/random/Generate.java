f6564dd02fb13d75ffe2ba922de5b7e40a12c25a
eltacshikhsaidov <eltac.shixseyidov250301@gmail.com>
2021-11-27 00:49:06 +0400

    fixing time range

--- src/me/eltacshikhsaidov/external/random/Generate.java
+++ src/me/eltacshikhsaidov/external/random/Generate.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Generate {
     public static int random() {
         Random random = new Random();
-        int low = 15; // from half hour 30 minutes
-        int high = 18; // to one hour and half 90 minutes
+        int low = 15;
+        int high = 18;
         return random.nextInt(high - low) + low;
     }
 }
